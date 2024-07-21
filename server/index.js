fefea9ab753fa1e8cb4fe4e23972d5a81328f881
sinjini05 <98803426+sinjini05@users.noreply.github.com>
2024-07-22 02:53:17 +0530

    added room, socket

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 const {Server} = require("socket.io");
 
-const io = new Server(8000);
+const io = new Server(8000,{
+    //cross origin site policy --> frontend and 
+    //backend running on diff servers so chrome blocking it for us
+    cors: true,
+});
 
-io.on("connection",socket=>{
-    console.log('Socket Connected', socket.io);
+//mapping from email to id
+const emailToSocketIdMap = new Map();
+const socketidtoEmailMap = new Map();
+
+io.on("connection",(socket) => {
+    console.log(`Socket Connected`, socket.id);
+    //user req
+    socket.on("room:join", (data) =>{
+        //will get these info from the user's data provided
+        const { email,room } = data
+        emailToSocketIdMap.set(email,socket.id);
+        socketidtoEmailMap.set(socket.id,email);
+        //existing user will get this event
+        io.to(room).emit("user:joined",{email, id: socket.id});
+        //next will join the other user
+        socket.join(room);
+        //granting join req for user(socket.id) and sending data
+        io.to(socket.id).emit("room:join", data);
+        console.log(data);
+    });
 });
